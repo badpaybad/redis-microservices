72a67438d5d8ba92f1898fc55697db0ee338fdc3
Nguyen Phan Du <badpaybad@gmail.com>
2016-09-19 23:04:18 +0700

    add CommandStatus.cs
    Signed-off-by: Nguyen Phan Du <badpaybad@gmail.com>

---
+++ RedisMicroservices.Core/Distributed/CommandStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisMicroservices.Core.Distributed
+{
+    public enum CommandStatus
+    {
+        None,
+        Pushed,
+        Pedding,
+        Success,
+        Error
+    }
+}
